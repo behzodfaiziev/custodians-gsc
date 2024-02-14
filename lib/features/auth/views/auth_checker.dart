10ced6a34551fc29bc93db7807f7acb3c29096bc
Behzod <behzod.faiziev@gmail.com>
2024-02-14 18:17:08 +0300

    added auth checker

---
+++ lib/features/auth/views/auth_checker.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AuthChecker extends StatelessWidget {
+  const AuthChecker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('AuthChecker'),
+      ),
+    );
+  }
+}
