461f538a8eed0e3362b1e02e7b192d8779b8e393
Behzod <behzod.faiziev@gmail.com>
2024-02-14 13:24:23 +0300

    fixed errors

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 extension ContextExtension on BuildContext {
--- lib/core/init/config/system_init.dart
+++ lib/core/init/config/system_init.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import '../../../firebase_options.dart';
 class SystemInit {
   static SystemInit? _instance;
 
@@ -13,8 +11,8 @@ class SystemInit {
     /// Needs to be called so that we can await for
     /// EasyLocalization.ensureInitialized();
     WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform);
+    // await Firebase.initializeApp(
+    //     options: DefaultFirebaseOptions.currentPlatform);
 
     /// Set orientation to portrait
     await SystemChrome.setPreferredOrientations([
