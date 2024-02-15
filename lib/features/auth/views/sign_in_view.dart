dcefe52cae118a9e70a0149ac4584b00572ba157
yldcelal <yelduscelal@gmail.com>
2024-02-16 00:23:33 +0300

    Update OnBoard

--- lib/features/auth/views/sign_in_view.dart
+++ lib/features/auth/views/sign_in_view.dart
@@ -9,11 +9,60 @@ class SignInView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: ElevatedButton(
-          child: const Text('Sign In'),
-          onPressed: () {
-            context.pushReplaceAll(const MainView());
-          }),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const SizedBox(
+            height: 5,
+          ),
+          const SizedBox(
+            child: Column(
+              children: [
+                Text(
+                  "Welcome To",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text("Custodians",
+                    style:
+                        TextStyle(fontSize: 30, fontWeight: FontWeight.w600)),
+              ],
+            ),
+          ),
+          ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(30)),
+            child: SizedBox(
+              height: 200,
+              width: 300,
+              child: Image.network(
+                "https://picsum.photos/200/300",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              context.pushReplaceAll(const MainView());
+            },
+            style: ElevatedButton.styleFrom(
+              elevation: 5,
+              minimumSize: const Size(350, 50),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            child: const Text(
+              "Sign In",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ],
+      ),
     ));
   }
 }
